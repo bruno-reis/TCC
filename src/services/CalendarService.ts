51dde5f7c7b3a5a0e0dabd8b01ce7a47bda3396c
bruno-reis <bruno.reis@useboom.net>
2016-09-25 20:58:41 -0300

    calendar now adds classes to calendar/need to fix start-end times

--- src/services/CalendarService.ts
+++ src/services/CalendarService.ts
@@ -16,26 +16,41 @@ class CalendarService {
     })
   }
 
+  mod(n, m) {
+    return ((n % m) + m) % m
+  }
+
   createClassEvents(input, startDate, endDate, name) {
     //TODO: Currently timezone is affecting the start/end of the events on calendar. Try to calculate days in another way
     startDate = new Date(startDate)
     endDate = new Date(endDate)
-    let currDate = startDate
-  
-  //Get the closest day to the subject startDay on a Date Object
-    while ( input.day != currDate.getDay()) {
-      currDate = new Date(currDate.getTime() + 24*3600000)
-    }
+    console.log("sd", startDate)
+
+    let diff = this.mod(input.day - startDate.getDay() , 6 )
+    
+    startDate.setDate( startDate.getDate() + diff)
+    console.log("sf", startDate)
+
+    let currDate = new Date()
+    currDate = startDate
+    //Get the closest day to the subject startDay on a Date Object
+    // while ( input.day != currDate.getDay()) {
+    //   currDate = new Date(currDate.getTime() + 24*3600000)
+    //   console.log("c", currDate)
+    // }
+
+
   //Increment currDate by 7 days until we get to the endDate.
   //So we add this class for the whole period on the calendar.
-    while ( currDate.getTime() <= endDate.getTime() ) {
+    while ( currDate <= endDate ) {
       this.events.push({
         title: name,
         startTime: currDate.getTime() + input.startTime.getTime(),
         endTime: currDate.getTime() + input.endTime.getTime(),
         allDay: false
       })
-      currDate = new Date(currDate.getTime() + 7*24*3600000)
+
+      currDate.setDate(currDate.getDate() + 7)
     }
   }
 
