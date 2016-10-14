e77a213831bc0e4de45c9dc16921bb9e015df1bd
Renato Massao <renato.massao@live.com>
2016-10-14 20:42:34 -0300

    Moving exam delete button to exam edition view

--- src/subject/subject.edit.exam.controller.ts
+++ src/subject/subject.edit.exam.controller.ts
@@ -3,21 +3,30 @@
 class subjectEditExamCtrl {
   public $inject = ['$stateParams', '$state', 'SubjectService']
   private input
+  private subjectId
+  private examId
 
   constructor(public $state,
               public $stateParams,
-              public SubjectService) {
-    var subjectId = this.$state.params['subjectId']
-    var examId = this.$state.params['examId']
-    this.input = this.SubjectService.getSubjectProperty(subjectId, examId, "exams")
+              public SubjectService,
+              public CalendarService) {
+    this.subjectId = this.$state.params['subjectId']
+    this.examId = this.$state.params['examId']
+    this.input = this.SubjectService.getSubjectProperty(this.subjectId, this.examId, "exams")
     this.input.date = new Date(this.input.date)
     this.input.startTime = new Date(this.input.startTime)
     this.input.endTime = new Date(this.input.endTime)
   }
 
   submit() {
-    this.SubjectService.editSubjectProperty(this.$state.params['subjectId'], "exams", this.input)
+    this.SubjectService.editSubjectProperty(this.subjectId, "exams", this.input)
     this.$state.go('.^.info')
+  }
+
+  delete() {
+    this.SubjectService.deleteSubjectProperty(this.subjectId, "exams", this.examId)
+    this.CalendarService.deleteChildEvent(this.subjectId, this.examId)
+    this.$state.go('.^.info', this.$stateParams, {reload: true, inherit: false});
   }
 }
 
