303799fe06c7772e472c290ca6ab548e3d4544f6
bruno-reis <bruno.reis@useboom.net>
2016-10-17 12:20:39 -0200

    added time validation to activity

--- src/activities/activities.add.day.controller.ts
+++ src/activities/activities.add.day.controller.ts
@@ -1,19 +1,32 @@
 /// <reference path="../../typings/tsd.d.ts" />
 
 class activitiesAddDayCtrl {
-  public $inject = ['$stateParams', '$state', 'ActivityService', 'CalendarService']
+  public $inject = ['$ionicPopup', '$stateParams', '$state', 'ActivityService', 'CalendarService', 'PopupService']
 
   private input
   private activity
 
   constructor(public $state,
               public $stateParams,
+              public $ionicPopup,
+              public PopupService,
               public ActivityService,
               public CalendarService) {
     this.activity = this.ActivityService.getActivity(this.$state.params['activityId'])
+    this.input = {}
+    this.input.day = '1'
+  }
+
+  validate() {
+    if (this.input.startTime >= this.input.endTime) {
+      this.PopupService.timeErrorPopup().then(() => this.input.endTime = null)
+      return false
+    }
+    return true
   }
 
   submit() {
+    if (!this.validate()) return
     this.ActivityService.addDay(this.$state.params['activityId'], this.input)
     this.CalendarService.createMultipleEvents(this.input, this.activity)
     this.$state.go('.^.info')
