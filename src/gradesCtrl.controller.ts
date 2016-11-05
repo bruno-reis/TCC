712d230bfbcf5d60d4160765fa2151f59e4492e8
bruno-reis <bruno.reis@useboom.net>
2016-11-05 20:49:29 -0200

    created controller that gets the list of exams/homework grades

---
+++ src/gradesCtrl.controller.ts
@@ -0,0 +1,23 @@
+/// <reference path="../typings/tsd.d.ts" />
+
+class GradesCtrl {
+  public $inject = ['$stateParams', '$state', 'SubjectService']
+  private subjects = []
+  private subjectsExams = []
+  private subjectsHomework = []
+
+  constructor(public $state,
+              public $stateParams,
+              public SubjectService) {
+    this.subjects = this.SubjectService.getSubjects()
+    this.subjects.map( sub => sub.exams.map( s => this.subjectsExams.push(s)))
+    this.subjects.map( sub => sub.homeworks.map( s => this.subjectsHomework.push(s)))
+  }
+
+  selectSubject(subjectId) {
+    this.$state.go('subject.info', {subjectId: subjectId})
+  }
+}
+
+angular.module('app.controllers')
+  .controller('gradesCtrl', GradesCtrl)
