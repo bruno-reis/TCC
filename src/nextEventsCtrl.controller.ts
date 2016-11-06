927568e17bcc412445188cd52c6a3834f525f5e4
bruno-reis <bruno.reis@useboom.net>
2016-11-06 16:28:11 -0200

    added methods to filter date range and select subject

--- src/nextEventsCtrl.controller.ts
+++ src/nextEventsCtrl.controller.ts
@@ -2,11 +2,26 @@
 
 class NextEventsCtrl {
   public $inject = ['$stateParams', '$state', 'SubjectService', 'CalendarService']
+  private events;
+  private nextEvents;
+  private range = "7";
 
   constructor(public $state,
               public $stateParams,
               public CalendarService,
               public SubjectService) {
+    this.events = this.CalendarService.getEvents().filter(e => e.type == "homeworks");
+    this.filterEvents()
+  }
+  
+  filterEvents() {
+    let start = new Date()
+    let end = new Date().setDate(start.getDate() + parseInt(this.range, 10))
+    this.nextEvents =  this.events.filter( ev => ev.startTime >= start && ev.endTime <= end)
+  }
+
+  selectSubject(subjectId) {
+    this.$state.go('subject.info', {subjectId: subjectId})
   }
   
 }
