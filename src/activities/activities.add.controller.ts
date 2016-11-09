650fc783a637b4410754a54bb64098b47fc49785
bruno-reis <bruno.reis@useboom.net>
2016-11-09 09:08:48 -0200

    added method to add a single activity/changed views to display single acitivity

--- src/activities/activities.add.controller.ts
+++ src/activities/activities.add.controller.ts
@@ -1,19 +1,38 @@
 /// <reference path="../../typings/tsd.d.ts" />
 
 class ActivitiesAddCtrl {
-  public $inject = ['$state', 'ActivityService', 'StorageService']
+  public $inject = ['$state', 'ActivityService', 'StorageService', 'PopupService', 'CalendarService']
   private input
-  private isRecurring = false
+  private date
+  private day
 
   constructor(public $state,
               public ActivityService,
+              public PopupService,
+              public CalendarService,
               public StorageService) {
     this.input = {}
     this.input.duration = '1'
+    this.input.day = '1'
+    this.input.recurring = false
   }
 
-  addActivity() {
-    this.ActivityService.addActivity(this.input)
+  validate() {
+    if (this.input.startTime >= this.input.endTime) {
+      this.PopupService.timeError().then(() => this.input.endTime = null)
+      return false
+    }
+    return true
+  }
+
+  submit() {
+    if (this.input.recurring) {
+      this.ActivityService.addActivity(this.input)
+    } else {
+      if (!this.validate()) return
+      this.ActivityService.addSingleActivity(this.input, this.day, this.date)
+      this.CalendarService.createEvent(this.day, this.input, this.date)
+    }
     this.$state.go('activities.list')
   }
 }
